83e67153b8c509e947186946f6f657bbc810785b
gauravsingh5009 <2000gauravjnp@gmail.com>
2024-11-06 19:00:37 +0530

    Update popup

--- jQuery/jsfile.js
+++ jQuery/jsfile.js
@@ -1,7 +1,53 @@
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn();
-    } else {
-      $('.back-to-top').fadeOut();
-    }
-  });
+$(function(){
+
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn();
+      } else {
+        $('.back-to-top').fadeOut();
+      }
+    });
+
+      // Open when click on select
+      $('#openModal').click(function() {
+        $('#myModal').fadeIn();  
+      });
+    
+      // Close the button
+      $('#closeModal').click(function() {
+        $('#myModal').fadeOut();  
+      });
+
+      // Open New Address 
+      $('#addNewAddress').click(function() {
+        $('#myModal').fadeOut();
+        $('#newAddress').fadeIn();  
+      });
+    
+      // BackButton
+      $('#backBtn').click(function() {
+        $('#newAddress').fadeOut();
+      });
+
+      // Search Location Manually
+      $('#selectManualArea').click(function() {
+        $('#newAddress').fadeOut();
+        $('#SearchLocation').fadeIn();
+      });
+
+      // Close Secrch Location
+      $('#closeSearchLocation').click(function() {
+        $('#SearchLocation').fadeOut();
+      });
+
+      // Detail Address
+      $('.selectLocalities').click(function() {
+        $('#SearchLocation').fadeOut();
+        $('#fullAddress').fadeIn();
+      })
+
+      // Close Detail Button
+      $('#closeDetailsBtn').click(function() {
+        $('#fullAddress').fadeOut();
+      })
+});
